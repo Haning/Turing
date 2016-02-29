2fdbcb6bb4e3b9a106de7d86bcb601675ed136e0
xuezeng Han <hanxuezeng.china@gmail.com>
2016-02-29 16:38:16 +0800

    Update DButil.py

--- TuringBook/DButil.py
+++ TuringBook/DButil.py
@@ -6,11 +6,22 @@ import os
 import TuringBook.Book
 
 
-DB_File_PATH = "\Book.db"
+DB_File_PATH = "E:\\Users\\xuezenghan\\PycharmProjects\\Turing\\Book.db"
 
 
 def init():
-    if(os.)
+
+    if not os.path.exists(DB_File_PATH):
+        db = sqlite3.connect(DB_File_PATH)
+        cur = db.cursor()
+        cur.execute("CREATE TABLE books(id integer primary key ,"
+                    "bookName VARCHAR(200) NOT NULL ,"
+                        "price INTEGER NOT NULL,NewPrice INTEGER NOT NULL,BookCoverURL VARCHAR(200) NOT NULL ,"
+                        "dateTime DATETIME DEFAULT(date('now')))")
+
+        cur.close()
+        db.close()
+
 
 
 
@@ -23,4 +34,43 @@ def get_conn():
 
 
 def saveBook(conn,book):
-    book.toJson()
+    try:
+        bookJson = book.toJson()
+        print(bookJson)
+        bookstr = (bookJson["BookName"],bookJson["price"],bookJson["NewPrice"],bookJson["BookCoverURL"])
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO books(bookName , price ,NewPrice,BookCoverURL) VALUES (?,?,?,?)",bookstr)
+        cur.close()
+        conn.commit()
+
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
+
+
+
+def searchBook(conn):
+
+    bookresult =[]
+    cur = conn.cursor()
+    cur.execute("select bookName , price ,NewPrice,BookCoverURL from books order by dateTime DESC limit 2")
+    for eachBook in cur.fetchall():
+        print(eachBook)
+        bookresult.append(eachBook)
+
+    cur.close
+    return bookresult
+
+
+def closeDB(conn):
+    conn.close()
+
+
+
+
+
+
+
